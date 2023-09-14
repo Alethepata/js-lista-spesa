386f34ea9af2f7d64e201ca7a9f887d1a9ee254a
Alethepata <alessia.patane@gmail.com>
2023-09-14 18:01:54 +0200

    [Add] While

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 const content = document.getElementById('list');
 
 const list = ['pane', 'pasta', 'salumi'];
+
+let i = 0;
+while( i < list.length){
+    console.log(list[i]);
+    i++        
+}
+
+
