2a95fced8ab68bccfd06848a9084208754c6656b
Alethepata <alessia.patane@gmail.com>
2023-09-15 13:15:35 +0200

    [Edit] While

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const content = document.getElementById('list');
 
-const list = ['pane', 'pasta', 'salumi'];
+const list = ['pane', 'pasta', 'salumi', 'bibite', 'fazzoletti', 'tonno'];
 
 let i = 0;
-while( i < list.length){
-    console.log(list[i]);
-    i++        
+while( i < list.length){ 
+    content.innerHTML += `<li class=" border-bottom list-group-item p-2 text-secondary">${list[i]}</li>`;      
+    i++ 
+    
 }
 
 
